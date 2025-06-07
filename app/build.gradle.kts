bb9c9481c4f4c10374e2ea8fd1190624cec55819
lior.dahan <lior.dahan@justeattakeaway.com>
2025-06-07 07:54:26 +0300

    bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.liordahan.mgsrteam"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "2.2"
+        versionCode = 5
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
