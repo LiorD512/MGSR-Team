b616edf981d9b328cc35ec850bc3f30032f6df6a
lior.dahan <lior.dahan@justeattakeaway.com>
2026-04-20 21:24:00 +0300

    v 1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.liordahan.mgsrteam"
         minSdk = 28
         targetSdk = 36
-        versionCode = 48
-        versionName = "1.5.1"
+        versionCode = 49
+        versionName = "1.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
